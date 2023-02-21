27108e5221a33c9b378d3827946e26ce3a3fb865
AndyMaa <3102480846@qq.com>
2023-02-21 20:20:42 +0800

    1

--- src/minigame/Main.java
+++ src/minigame/Main.java
@@ -1,7 +1,9 @@
 package minigame;
 
+import minigame.ui.GameFrame;
+
 public class Main {
     public static void main(String[] args) {
-
+        new GameFrame("MiniGame");
     }
 }
--- src/minigame/ui/GameFrame.java
+++ src/minigame/ui/GameFrame.java
@@ -7,4 +7,11 @@ import javax.swing.*;
  * 自己实现
  */
 public class GameFrame extends JFrame {
+    public GameFrame(String name){
+        super(name);
+        setSize(500, 500);
+        setResizable(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
 }
