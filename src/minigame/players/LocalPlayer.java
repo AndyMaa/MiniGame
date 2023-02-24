4f15b3788e2396eb4e795c582c896f541cf98df2
AndyMaa <3102480846@qq.com>
2023-02-24 14:34:33 +0800

    Update LocalPlayer.java

--- src/minigame/players/LocalPlayer.java
+++ src/minigame/players/LocalPlayer.java
@@ -2,10 +2,14 @@ package minigame.players;
 
 import minigame.core.server.Server;
 
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
 /**
  * 本机玩家
  */
-public class LocalPlayer implements Player{
+public class LocalPlayer implements Player, MouseListener {
     @Override
     public void step(int x, int y) {
 
@@ -24,5 +28,30 @@ public class LocalPlayer implements Player{
     @Override
     public int getId() {
         return 0;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
     }
 }
