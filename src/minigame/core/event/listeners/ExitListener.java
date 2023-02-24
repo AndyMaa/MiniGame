1d85848999a047e75daa55b48ac3b6bfd2111b83
AndyMaa <3102480846@qq.com>
2023-02-24 18:32:02 +0800

    学校没传的

---
+++ src/minigame/core/event/listeners/ExitListener.java
@@ -0,0 +1,13 @@
+package minigame.core.event.listeners;
+
+import minigame.ui.GameFrame;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ExitListener implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        GameFrame.instance.setMode("welcome");
+    }
+}
---
+++ src/minigame/ui/buttons/More.java
@@ -0,0 +1,16 @@
+package minigame.ui.buttons;
+
+import javax.swing.*;
+
+/**
+ * 更多按钮
+ */
+public class More extends JButton {
+    public static JButton EXIT=new JButton("退出");
+    public static JButton REGRET=new JButton("悔棋");
+
+    public More(){
+
+    }
+
+}
