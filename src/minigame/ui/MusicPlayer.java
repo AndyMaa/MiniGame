bc36c89243e62f5b5aff45826ccafd1b65a195fd
BobbyWch <103981567+BobbyWch@users.noreply.github.com>
2023-03-02 12:23:53 +0800

    temp

--- src/minigame/ui/MusicPlayer.java
+++ src/minigame/ui/MusicPlayer.java
@@ -1,16 +1,33 @@
 package minigame.ui;
 
-import javafx.scene.media.AudioClip;
-
 import javax.sound.sampled.*;
+import java.applet.Applet;
+import java.applet.AudioClip;
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class MusicPlayer {
     private static byte[] music;
     private static AudioFormat format;
     public static void playBackground(){
-        AudioClip clip=new AudioClip(MusicPlayer.class.getClassLoader().getResource("res/music.ogg").toString());
+        try {
+            InputStream is=MusicPlayer.class.getClassLoader().getResource("res/music.ogg").openStream();
+            BufferedInputStream bis=new BufferedInputStream(is);
+            bis.mark(1024);
+            AudioInputStream ais=AudioSystem.getAudioInputStream(bis);
+        } catch (IOException | UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        }
+        AudioClip clip= Applet.newAudioClip(MusicPlayer.class.getClassLoader().getResource("res/music.wav"));
+//        clip.
         clip.play();
+        System.out.println("wait");
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
